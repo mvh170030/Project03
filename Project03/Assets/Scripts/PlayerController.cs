96a7270005802f25f471b3247b19909cd0d2cc21
David Hernandez <mvh170030@utdallas.edu>
2020-11-15 18:35:19 -0600

    Player movement

---
+++ Project03/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    private Rigidbody2D myRigidbody;
+    private Animator myAnimator;
+
+    public bool sliding;
+    public bool grounded;
+    public LayerMask whatIsGround;
+    private Collider2D myCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (grounded)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.RightShift) || Input.GetMouseButton(1))
+        {
+            sliding = true;
+        } else { sliding = false; }
+
+        myAnimator.SetFloat("Speed", myRigidbody.velocity.x);
+        myAnimator.SetBool("Grounded", grounded);
+        myAnimator.SetBool("Sliding", sliding);
+    }
+}
